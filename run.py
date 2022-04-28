cb5ecf26ff08e4a6a7c1aa1abab88a9e6a3de4c4
ShonaOB <shona-obriendev@outlook.com>
2022-04-28 09:20:13 +0000

    Create get_delivery_date function to get the delivery date from the user

--- run.py
+++ run.py
@@ -22,7 +22,24 @@ def get_school_name():
     print("for example: Dublin St. Mary's NS \n")
 
     school_name_str = input("Enter your school name here:\n")
-    print(f"The school name you have provided is {school_name_str}")
+    print(f"The school name you have provided is {school_name_str}\n")
+    return school_name_str
+
+
+def get_delivery_date():
+    """
+    Get the date of the delivery from the user
+    """
+    print("Please enter the required delivery date for this order")
+    print("The date should be in the format DD/MM/YYYY")
+    print("for example: 01/04/2022 \n")
+
+    delivery_date = input("Enter your required delivery date:\n")
+    print(f"You have requested a delivery on the {delivery_date}\n")
+
+
+
 
 
 get_school_name()
+get_delivery_date()
