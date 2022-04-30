8ab4060e224dd0e0d03de41ba0cf472dca45f89b
ShonaOB <shona-obriendev@outlook.com>
2022-04-30 18:40:02 +0000

    Add date validation code to get_delivery_date() and test thoroughly

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
+import random
+import datetime
 import gspread
 from google.oauth2.service_account import Credentials
-import datetime
-import random
-
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,33 +22,40 @@ def get_school_name():
     global school_name_str
     global county
     print("Please enter the school name")
-    print("The name of the school should start with the County.")
-    print("Please note! Delivery is currently only available in Dublin and Offaly.")
-    print("for example: Dublin St. Mary's NS \n")
+    print("The name of the school should start with the County.\n")
+    print("Please note! Delivery is currently")
+    print("only available in Dublin and Offaly.")
+    print("for example: Dublin St. Mary's NS ")
 
     school_name_str = input("Enter your school name here:\n").capitalize()
     split = school_name_str.split()
     county = split[0]
     client_counties = ["Offaly", "Dublin"]
-    
+
     if any(county in school_name_str for county in client_counties):
         print(f"Great! We deliver to that county! {county}\n")
-    else: 
-        print("Oops! You did not enter a county, or you entered a county that we do not deliver to! Try again...\n")
+    else:
+        print("Oops! You did not enter a county\n")
+        print("or you entered a county that we do not deliver to!")
+        print("Try again...\n")
         main()
 
     print(f"The school name you have provided is {school_name_str}\n")
     return school_name_str
 
 
-def production_run(): 
-    random_no = random.randint(0,200)
+def production_run():
+    """
+    Assigns a random number and the county to create a production run
+    """
+    random_no = random.randint(0, 200)
     prun = county + " " + str(random_no)
-    print(prun) 
+    print(prun)
+
 
 def get_delivery_date():
     """
-    Get the date of the delivery from the user
+    Get the date of the delivery from the user and validate it also
     """
     print("Please enter the required delivery date for this order")
     print("The date should be in the format DD/MM/YYYY")
@@ -57,10 +63,40 @@ def get_delivery_date():
 
     global delivery_date
     delivery_date = input("Enter your required delivery date:\n")
-    # CREDIT: this code to validate the date was taken from tutsmake.com 
+    # CREDIT: this code to validate the date was taken from tutsmake.com
     # see README for details
-    
-    print(f"You have requested a delivery on the {delivery_date}\n")
+    dd,mm,yy=delivery_date.split('/')
+    dd=int(dd)
+    mm=int(mm)
+    yy=int(yy)
+    if(mm==1 or mm==3 or mm==5 or mm==7 or mm==8 or mm==10 or mm==12):
+        max1=31
+    elif(mm==4 or mm==6 or mm==9 or mm==11):
+        max1=30
+    elif(yy%4==0 and yy%100!=0 or yy%400==0):
+        max1=29
+    else:
+        max1=28
+    if(mm<1 or mm>12):
+        print("Date is invalid.")
+        get_delivery_date()
+    elif(dd<1 or dd>max1):
+        print("Date is invalid.")
+        get_delivery_date()
+    elif(dd==max1 and mm!=12):
+        dd=1
+        mm=mm+1
+        print("The date is: ",dd,mm,yy)
+    elif(dd==31 and mm==12):
+        dd=1
+        mm=1
+        yy=yy+1
+        print("The date is: ",dd,mm,yy)
+    else:
+	    dd=dd
+	    print("The delivery date is: ",dd,mm,yy)
+
+    #print(f"You have requested a delivery on the {delivery_date}\n")
     return delivery_date
 
 
@@ -86,6 +122,9 @@ def update_worksheet(data):
 
 
 def main():
+    """
+    Runs the main program
+    """
     data = [get_school_name(), production_run(), get_delivery_date(), get_order_detail()]
     update_worksheet(data)
 
